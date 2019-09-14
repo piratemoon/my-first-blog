37d0cc96f07e2491bf1f39af71c67ae414b0142c
piratemoon <jenny.walklate@gmail.com>
2019-09-14 11:28:24 +0100

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class  Post(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save
+
+	def __str__(self):
+		return self.title
+	"""docstring for  Post"models.Modelf 
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	tle = models.CharField(max_length=200)
+	xt = models.TextField()
+	eated_date = models.DateTimeField(default=timezone.now)__init__(self, arg):
+		super( Post,models.Model._
+		author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+		tle = models.CharField(max_length=200)
+		xt = models.TextField()
+		eated_date = models.DateTimeField(default=timezone.now)_init__()
+		self.arg = arg
+		"""
